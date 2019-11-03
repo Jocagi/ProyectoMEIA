a64e07171bcda5499f7be49af4e48500273d0dc7
JOSE CARLOS GIRON MARQUEZ <jcgironma@correo.url.edu.gt>
2019-11-02 23:03:16 -0600

    Reestructuracion Clase ArchivoSecuencial

---
+++ ProyectoMEIA/src/main/java/Classes/BackUp.java
@@ -0,0 +1,77 @@
+package Classes;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class BackUp {
+    
+  public static void MoverCarpeta(String Origen, String Destino)
+  {
+      try {
+          File sourceFolder = new File(Origen);
+         
+        //Target directory where files should be copied
+        File destinationFolder = new File(Destino);
+ 
+        //Call Copy function
+        copyFolder(sourceFolder, destinationFolder);            
+        JOptionPane.showMessageDialog(null, "BackUp Succesful");
+            
+        } catch (IOException ex) {
+            
+            JOptionPane.showMessageDialog(null, "Couldnt copy files");
+        }
+  }
+  private static void copyFolder(File sourceFolder, File destinationFolder) throws IOException
+    {
+        //Check if sourceFolder is a directory or file
+        //If sourceFolder is file; then copy the file directly to new location
+        if (sourceFolder.isDirectory())
+        {
+            //Verify if destinationFolder is already present; If not then create it
+            if (!destinationFolder.exists())
+            {
+                destinationFolder.mkdir();
+            }
+             
+            //Get all files from source directory
+            String files[] = sourceFolder.list();
+             
+            //Iterate over all files and copy them to destinationFolder one by one
+            for (String file : files)
+            {
+                File srcFile = new File(sourceFolder, file);
+                File destFile = new File(destinationFolder, file);
+                 
+                //Recursive function call
+                copyFolder(srcFile, destFile);
+            }
+        }
+        else
+        {
+            //Copy the file content from one place to another
+            Files.copy(sourceFolder.toPath(), destinationFolder.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        }
+    }
+}
+ 
+
